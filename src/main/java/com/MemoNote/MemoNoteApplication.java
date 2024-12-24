6330823a28165dd357eaadffd74720c9aca447f9
megait202 <stay488@gmail.com>
2024-12-24 18:24:39 +0900

    [신재경] 프로젝트 생성

---
+++ src/main/java/com/MemoNote/MemoNoteApplication.java
@@ -0,0 +1,13 @@
+package com.MemoNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MemoNote/MemoNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MemoNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
